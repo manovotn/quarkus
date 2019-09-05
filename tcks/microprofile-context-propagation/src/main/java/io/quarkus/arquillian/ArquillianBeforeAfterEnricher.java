afb2275a0158f2dcfa93eb5a668edf7aa4cc5a65
Matej Novotny <manovotn@redhat.com>
2019-09-05 18:00:47 +0200

    Added Arq. extension that activates request context.

---
+++ tcks/microprofile-context-propagation/src/main/java/io/quarkus/arquillian/ArquillianBeforeAfterEnricher.java
@@ -0,0 +1,31 @@
+package io.quarkus.arquillian;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.arc.ArcContainer;
+import org.jboss.arquillian.core.api.annotation.Observes;
+
+/**
+ * Activates request context before test runs and shuts it down afterwards
+ */
+public class ArquillianBeforeAfterEnricher {
+
+    private static final String ERROR_MSG = "Arc container is not running, cannot activate CDI contexts!";
+
+    public void on(@Observes(precedence = -100) org.jboss.arquillian.test.spi.event.suite.Before event) throws Throwable {
+        ArcContainer container = Arc.container();
+        if (container.isRunning()) {
+            container.requestContext().activate();
+        } else {
+            throw new IllegalStateException(ERROR_MSG);
+        }
+    }
+
+    public void on(@Observes(precedence = 100) org.jboss.arquillian.test.spi.event.suite.After event) throws Throwable {
+        ArcContainer container = Arc.container();
+        if (container.isRunning()) {
+            container.requestContext().terminate();
+        } else {
+            throw new IllegalStateException(ERROR_MSG);
+        }
+    }
+}
